9cf22269a2a35d32a70adbed0d8a05f1f95462ac
Sylvian <ngocthai0789@gmail.com>
2021-09-17 15:06:03 +0700

    Add Commit

---
+++ StarCounter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace StarCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            if (args.Length > 0)
+                n = int.Parse(args[0]);
+            else
+                n = 1;
+            string s = "";
+            for(int i=0;i<n;i++)
+            {
+                s += "*";
+                Console.WriteLine(s);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
